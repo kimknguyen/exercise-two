3c33434c3a4e0e0ced4732d972bbec129ac1dd0a
Kim Nguyen <kkn2011@nyu.edu>
2023-09-19 08:46:18 -0400

    first commit

---
+++ src/app/pages/index.js
@@ -0,0 +1,11 @@
+import style from "../app/page.module.css";
+
+export default function Home(){
+    const projectName = "Exercise Two";
+    return (
+        <main className ={StyleSheet.main}>
+            <h1>{projectName}</h1>
+        </main>
+        
+    );
+}
